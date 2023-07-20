5b0bc8278904c7bd4da545570e7b75f9a42f6cfc
ashishchaudhary66 <ashishkumarchaudhary66@gmail.com>
2023-07-20 15:33:51 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        Whiney:["Whitney"],
+        Ginto:["Ginto"],
+        ggSans:["ggSans"],
+        GintoHeading:["ABC Ginto Nord","Noto Sans","Helvetica Neue","Helvetica","Arial","sans-serif"]
+      },
+    },
+  },
+  plugins: [],
+}
+
